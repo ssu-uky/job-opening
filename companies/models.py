3ff1aecc262005fedd9ec220f58dbeaf5d21a8fb
ssu-uky <id_suhyun@naver.com>
2023-10-13 19:25:35 +0900

    chore: 모델 설명 추가

--- companies/models.py
+++ companies/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class Company(models.Model):
+    """회사 모델"""
+
     company_name = models.CharField(max_length=200, unique=True)
     address = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.company_name
--- recruits/models.py
+++ recruits/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 class Recruit(models.Model):
+    """채용공고 모델"""
+
     company = models.ForeignKey("companies.Company", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     position = models.CharField(max_length=200)
     reward = models.PositiveBigIntegerField()
     skill = models.CharField(max_length=200)
     content = models.TextField()
-    
+
     def __str__(self):
         return self.title
